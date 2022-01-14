1979c2d77b51950d928eff2c01872a2e51396efd
lucas.caminha <lucas_santoscaminha@hotmail.com>
2022-01-13 21:11:08 -0300

    documentação da api com swagger

--- src/main/java/br/com/agenda/SecurityConfiguration.java
+++ src/main/java/br/com/agenda/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/user/registrar").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.anyRequest().authenticated()
+		.anyRequest().permitAll()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().addFilterBefore(new AuthenticationTokenFilter(tokenService, userRepository), UsernamePasswordAuthenticationFilter.class);
